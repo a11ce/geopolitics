ac633f271aeca360d77bcb31f4516790bdc6db99
a11ce <a11ce@protonmail.com>
2018-10-14 22:20:00 -0400

    Fix CSS for GitHub Pages

--- build.sh
+++ build.sh
@@ -1,7 +1,8 @@
 #!/usr/bin/env bash
 
+cp pandoc.css docs/pandoc-built.css
 for i in *.rst; do
     fileName=${i%.rst}
-    pandoc -s -o docs/$fileName.html --css ../pandoc.css $fileName.rst
+    pandoc -s -o docs/$fileName.html --css pandoc-built.css $fileName.rst
 done
 pandoc -o README.md 1-intro.rst
