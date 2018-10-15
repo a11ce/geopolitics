b53fc2050f4e6d80c9d65dddb027329d5dcfb018
a11ce <a11ce@protonmail.com>
2018-10-14 22:40:23 -0400

    Build constants
    and spelling mistake

--- build.sh
+++ build.sh
@@ -1,5 +1,10 @@
 #!/usr/bin/env bash
 
+rm docs/*
+
+cp build-constants/index.html docs/index.html
+cp build-constants/pandoc.css docs/pandoc.css
+
 for i in *.rst; do
     fileName=${i%.rst}
     cat $fileName.rst footer.rst |  pandoc -s -o docs/$fileName.html --css pandoc.css -f rst
